d508ed60cdb438c3c11e2770dcdb42c4cd43086c
Sagar <sagar.k@dreamsgate.com>
2019-09-09 20:07:28 +0530

    Updated for mac

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/sagarkishore/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/sagarkishore/.fzf/bin"
+if [[ ! "$PATH" == */Users/apple/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/Users/apple/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/sagarkishore/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/Users/apple/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/sagarkishore/.fzf/shell/key-bindings.bash"
+source "/Users/apple/.fzf/shell/key-bindings.bash"
